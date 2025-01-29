09abd619a3bb121d9879994f9bebd5c5cac9c519
Zachary LaCore <zlacore12@gmail.com>
2025-01-28 19:04:15 -0600

    hi

---
+++ client/vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'https://test-deploy-ci.onrender.com',
+        secure: false,
+        changeOrigin: true,
+      },
+    },
+  },
+});
